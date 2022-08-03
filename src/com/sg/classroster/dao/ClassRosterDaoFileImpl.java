67a32f00014eb7bc62f39a1406a29002c6921c6b
Andrew Pomerleaum <arpomerleau@gmail.com>
2022-08-03 11:51:56 -0400

    FileImpl stores student data in a HashMap<String, Student>

--- src/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
+++ src/com/sg/classroster/dao/ClassRosterDaoFileImpl.java
@@ -1,13 +1,17 @@
 package com.sg.classroster.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.sg.classroster.dto.Student;
 
 public class ClassRosterDaoFileImpl implements ClassRosterDao {
+    final Map<String, Student> students = new HashMap<>();
+
     @Override
     public Student addStudent(String studentId, Student student) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return students.put(studentId, student);
     }
 
     @Override
