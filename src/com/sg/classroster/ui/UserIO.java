3d56aadaebd4e5515e376c9b7093877a20761389
Andrew Pomerleaum <arpomerleau@gmail.com>
2022-08-03 09:27:46 -0400

    Reorganize definitions

--- src/com/sg/classroster/ui/UserIO.java
+++ src/com/sg/classroster/ui/UserIO.java
@@ -3,6 +3,12 @@ package com.sg.classroster.ui;
 public interface UserIO {
     void print(String message);
 
+    public String readString(String prompt);
+
+    int readInt(String prompt);
+
+    int readInt(String prompt, int min, int max);
+
     double readDouble(String prompt);
 
     double readDouble(String prompt, double min, double max);
@@ -11,15 +17,9 @@ public interface UserIO {
 
     float readFloat(String prompt, float min, float max);
 
-    int readInt(String prompt);
-
-    int readInt(String prompt, int min, int max);
-
     long readLong(String prompt);
 
     long readLong(String prompt, long min, long max);
-
-    public String readString(String prompt);
 
     public void close();
 }
